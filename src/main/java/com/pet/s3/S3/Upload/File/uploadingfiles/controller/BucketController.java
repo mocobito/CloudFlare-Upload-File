f5c6dcab34a519e457fb5738f126379bf3c1fbe6
tobi <tai.mongtri@gmail.com>
2024-03-03 06:40:19 +0700

    nothing

--- src/main/java/com/pet/s3/S3/Upload/File/uploadingfiles/controller/BucketController.java
+++ src/main/java/com/pet/s3/S3/Upload/File/uploadingfiles/controller/BucketController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
+//@Log
 @RestController
 @RequestMapping("/api/storage/")
 public class BucketController {
