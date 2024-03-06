1cc24ecccf99fabb2c8c54da3eeb713e99ea3ca5
mttai <mttai@cmcglobal.vn>
2024-03-06 15:21:13 +0700

    finished

--- src/main/java/com/pet/s3/S3/Upload/File/uploadingfiles/service/AmazonClient.java
+++ src/main/java/com/pet/s3/S3/Upload/File/uploadingfiles/service/AmazonClient.java
@@ -1,25 +1,27 @@
 package com.pet.s3.S3.Upload.File.uploadingfiles.service;
 
+
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.*;
 import jakarta.annotation.PostConstruct;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Date;
 import java.util.Objects;
 
 @Service
+@Log4j2
 public class AmazonClient {
-
-    private AmazonS3 s3client;
 
     @Value("${amazonProperties.endpointUrl}")
     private String endpointUrl;
@@ -29,36 +31,54 @@ public class AmazonClient {
     private String accessKey;
     @Value("${amazonProperties.secretKey}")
     private String secretKey;
+    private AmazonS3 s3Client;
+
+    @Value("${amazonProperties.region}")
+    private String region;
+
+    @Value("${amazonProperties.signingRegion}")
+    private String signingRegion;
+
+    @Value("${amazonProperties.publicUrl}")
+    private String publicUrl;
 
     @PostConstruct
     private void initializeAmazon() {
+        log.info("---------------------------------");
+        log.info("Initializing Connect CloudFlare bucket name: " + bucketName);
+        log.info("---------------------------------");
         AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-        this.s3client = new AmazonS3Client(credentials);
+        this.s3Client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(endpointUrl, signingRegion))
+                .build();
     }
-    public String uploadFile(MultipartFile multipartFile) {
 
-        String fileUrl = "";
-        try {
-            File file = convertMultiPartToFile(multipartFile);
-            String fileName = generateFileName(multipartFile);
-            fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
-            uploadFileTos3bucket(fileName, file);
-            file.delete();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return fileUrl;
+    public String uploadFile(MultipartFile multipartFile)
+             {
+        File file = convertMultipartToFile(multipartFile);
+        String fileName = generateFileName(multipartFile);
+//        String fileUrl = endpointUrl.replace("$1", bucketName).replace("$2", region) + "/" + fileName;
+//        log.info("Uploading file to AWS S3: " + fileUrl);
+        return uploadFileToS3Bucket(bucketName, fileName, file);
     }
-    private void uploadFileTos3bucket(String fileName, File file) {
-        s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
-                .withCannedAcl(CannedAccessControlList.PublicRead));
+
+    private String uploadFileToS3Bucket(String bucketName, String fileName, File file) {
+        s3Client.putObject(
+                new PutObjectRequest(bucketName, fileName, file).withCannedAcl(CannedAccessControlList.PublicRead));
+        log.info("Public Url image to S3: " + publicUrl + fileName);
+        return publicUrl + fileName;
     }
-    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+
+    @SneakyThrows
+    private File convertMultipartToFile(MultipartFile file)  {
         File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
-        file.transferTo(convertedFile);
+        FileOutputStream fos = new FileOutputStream(convertedFile);
+        fos.write(file.getBytes());
+        fos.close();
         return convertedFile;
     }
+
     private String generateFileName(MultipartFile multiPart) {
-        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
+        return new Date().getTime() + "-" + Objects.requireNonNull(multiPart.getOriginalFilename()).replace(" ", "_");
     }
 }
